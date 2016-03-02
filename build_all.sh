48e19d5cd822a5c498cbed943837b8bc1d84a808
vance <vance.dubberly@ff0000.com>
2016-03-02 11:22:35 -0800

    simple script to rebuild everything from the ground up.

---
+++ build_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update all the docker images.
+# This script pulls the latest debian.  Builds base-machine and then builds all the images that 
+# extend base-machine. It should be run everytime you  make an update to base
+
+docker pull debian:8
+cd base
+docker build --no-cache -t=ff0000/base-machine:latest base-machine
+
+cd ../db
+docker build --no-cache -t=ff0000/db-machine:latest db-machine
+
+cd ../dev
+docker build --no-cache -t=ff0000/dev-machine:0.2 dev-machine
+
+cd ../app
+docker build --no-cache -t=ff0000/app-machine:0.2 app-machine
+
+cd ../lb
+docker build --no-cache -t=ff0000/lb-machine:0.2 lb-machine
