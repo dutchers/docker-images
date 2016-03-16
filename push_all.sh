a80b1b218c0a90bc84e67523aa28517164d42f38
vance <vance.dubberly@ff0000.com>
2016-03-16 14:15:27 -0700

    nice to not have to type this all over and over and over and over

---
+++ push_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Update all the docker images.
+# This script pulls the latest debian.  Builds base-machine and then builds all the images that 
+# extend base-machine. It should be run everytime you  make an update to base
+
+docker push ff0000/base-machine:latest
+
+docker push ff0000/db-machine:latest
+
+docker push ff0000/dev-machine:0.2
+
+docker push ff0000/app-machine:latest
+
+docker push ff0000/lb-machine:latest
